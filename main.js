1fe6c630030f8c33a796332b6a052f07d66aef4d
Aalia09 <76902204+Aalia09@users.noreply.github.com>
2021-03-24 16:03:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ menu_list_array=["Chicken Tandoori Pizza" ,
 function getmenu(){
  var htmldata;
  htmldata ="<ol class='menu_list'>"
- manu_list_array.sort();
+ menu_list_array.sort();
 
  for ( var i=0; i<menu_list_array.length;i++){
      htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
